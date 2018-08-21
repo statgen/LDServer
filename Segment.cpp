0e555b3c106d54dec3f56a7a1bf6b161a2389212
dtaliun <dtaliun@umich.edu>
2018-08-21 08:48:48 -0400

    delete old

--- Segment.cpp
+++
@@ -1,9 +0,0 @@
-#include "Segment.h"
-
-Segment::Segment(const string& chromosome, uint64_t start_bp, uint64_t stop_bp) : chromosome(chromosome), start_bp(start_bp), stop_bp(stop_bp) {
-
-}
-
-Segment::~Segment() {
-
-}
