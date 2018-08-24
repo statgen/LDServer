0d4f5b542a131e5c5fd03688b88f88b41375bd49
dtaliun <dtaliun@umich.edu>
2018-08-24 16:27:40 -0400

    add default config

--- rest/rest/__init__.py
+++ rest/rest/__init__.py
@@ -4,6 +4,7 @@ def create_app(test_config = None):
     app = Flask(__name__, instance_relative_config = True)
 
     if test_config is None:
+        app.config.from_object('config.default')
         app.config.from_pyfile('config.py', silent = True)
         app.config.from_envvar('RESTLD_CONFIG_FILE', silent = True)
     else:
