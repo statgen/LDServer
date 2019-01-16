3f3b66216b6fe6758db125cb2eb77723eb1f366c
Ryan Welch <welchr@umich.edu>
2019-01-16 10:00:04 -0500

    Fix spaces

--- rest/portal/__init__.py
+++ rest/portal/__init__.py
@@ -1,38 +1,38 @@
 from flask import Flask
 
 def create_app(test_config = None):
-    app = Flask(__name__, instance_relative_config = True)
+  app = Flask(__name__, instance_relative_config = True)
 
-    if test_config is None:
-        app.config.from_object('config.default')
-        app.config.from_pyfile('config.py', silent = True)
-        app.config.from_envvar('RESTLD_CONFIG_FILE', silent = True)
-    else:
-        app.config.from_mapping(test_config)
+  if test_config is None:
+    app.config.from_object('config.default')
+    app.config.from_pyfile('config.py', silent = True)
+    app.config.from_envvar('RESTLD_CONFIG_FILE', silent = True)
+  else:
+    app.config.from_mapping(test_config)
 
-    from .model import db, load_correlations, load_genotype_datasets_command, show_genotype_datasets_command, \
-        add_genotype_dataset_command, create_subset_command, show_genotypes_command, show_samples_command, \
-        add_phenotypes_command
-    
-    db.init_app(app)
-    app.cli.add_command(load_genotype_datasets_command)
-    app.cli.add_command(show_genotype_datasets_command)
-    app.cli.add_command(add_genotype_dataset_command)
-    app.cli.add_command(add_phenotypes_command)
-    app.cli.add_command(create_subset_command)
-    app.cli.add_command(show_genotypes_command)
-    app.cli.add_command(show_samples_command)
+  from .model import db, load_correlations, load_genotype_datasets_command, show_genotype_datasets_command, \
+    add_genotype_dataset_command, create_subset_command, show_genotypes_command, show_samples_command, \
+    add_phenotypes_command
 
-    from rest import api
-    app.register_blueprint(api.bp)
+  db.init_app(app)
+  app.cli.add_command(load_genotype_datasets_command)
+  app.cli.add_command(show_genotype_datasets_command)
+  app.cli.add_command(add_genotype_dataset_command)
+  app.cli.add_command(add_phenotypes_command)
+  app.cli.add_command(create_subset_command)
+  app.cli.add_command(show_genotypes_command)
+  app.cli.add_command(show_samples_command)
 
-    if app.config['GZIP_COMPRESSION']:
-        app.config['COMPRESS_MIMETYPES'] = ['application/json']
-        app.config['COMPRESS_LEVEL'] = 3
-        app.config['COMPRESS_MIN_SIZE'] = 500
-        api.compress.init_app(app)
+  from rest import api
+  app.register_blueprint(api.bp)
 
-    with app.app_context():
-        app.config['CORRELATIONS'] = load_correlations()
+  if app.config['GZIP_COMPRESSION']:
+    app.config['COMPRESS_MIMETYPES'] = ['application/json']
+    app.config['COMPRESS_LEVEL'] = 3
+    app.config['COMPRESS_MIN_SIZE'] = 500
+    api.compress.init_app(app)
 
-    return app
+  with app.app_context():
+    app.config['CORRELATIONS'] = load_correlations()
+
+  return app
