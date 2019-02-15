2321bd934d98d4a48a3f5243040f1980b820dca4
Ryan Welch <welchr@umich.edu>
2019-02-15 14:22:05 -0500

    Updated mask code

--- ld/src/Mask.h
+++ ld/src/Mask.h
@@ -1,15 +1,41 @@
 #ifndef LDSERVER_MASK_H
 #define LDSERVER_MASK_H
 
+#include <memory>
 #include <string>
 #include <map>
 #include <vector>
 #include <regex>
 #include <iostream>
 #include <set>
+#include <algorithm>
+#include <iterator>
 #include <tabixpp.hpp>
+#include "Types.h"
+
+// TODO: move to Types.h
+template<typename T>
+struct VariantSort {
+  bool operator()(const T& lhs, const T& rhs) const {
+    return lhs.position < rhs.position;
+  }
+};
+
+typedef std::set<VariantMeta, VariantSort<VariantMeta>> SortedVariantSet;
+
+struct MaskGroup {
+  std::string name;
+  std::string chrom;
+  uint64_t start;
+  uint64_t stop;
+  SortedVariantSet variants;
+
+  std::shared_ptr<std::set<std::string>> get_variants() const;
+};
 
 class Mask {
+  using group_iterator = std::map<std::string, MaskGroup>::const_iterator;
+
 public:
   /**
    * Default constructor that loads the entire mask.
@@ -24,7 +50,7 @@ public:
    * @param start
    * @param stop
    */
-  Mask(const std::string &filepath, const std::string &chrom, uint64_t &start, uint64_t &stop);
+  Mask(const std::string &filepath, const std::string &chrom, uint64_t start, uint64_t stop);
 
   /**
    * Print out each group and its variants, mainly for debugging purposes.
@@ -32,17 +58,38 @@ public:
   void print_groups() const;
 
   /**
+   * Get a list of group names.
+   */
+  std::shared_ptr<std::vector<std::string>> get_group_names() const;
+
+  /**
+   * Retrieve a group.
+   */
+  std::shared_ptr<MaskGroup> get_group(const std::string& group) const;
+
+  /**
    * Functions for retrieving the variants in a group.
    * @param group The group name.
    */
-  shared_ptr<std::set<std::string>> get_variant_set(const std::string &group) const;
-  shared_ptr<std::vector<std::string>> get_variant_vector(const std::string &group) const;
+  std::shared_ptr<std::set<std::string>> get_variant_set(const std::string &group) const;
+
+  /**
+   * Iterator functions.
+   * The iterator will have first and second members, with the first member
+   * being the name of the group, and the second member being a MaskGroup object.
+   */
+  group_iterator begin() const;
+  group_iterator end() const;
 
 protected:
+  std::string name;
+  uint64_t id;
+  std::string description;
+
   /**
    * Store map from group name --> vector of variants.
    */
-  std::map<std::string, std::vector<std::string>> groups;
+  std::map<std::string, MaskGroup> groups;
 
   /**
    * Loader for mask files. Expects the file to be tabixed and bgzipped.
