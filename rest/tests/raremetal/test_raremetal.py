16b514c2283c28c09d3f3734efab8a96f4d043eb
Ryan Welch <welchr@umich.edu>
2019-02-27 17:59:57 -0500

    flask test - covar shape

--- rest/tests/raremetal/test_raremetal.py
+++ rest/tests/raremetal/test_raremetal.py
@@ -16,3 +16,29 @@ def test_malformed_chrom(client):
     assert resp.status_code == 400
     assert resp.is_json
     assert re.search("Chromosome.*.not found",resp.json["error"]) is not None
+
+def test_covar(client):
+    resp = client.post("/aggregation/covariance", data = {
+        "chrom": "22",
+        "start": 50276998,
+        "stop": 50357719,
+        "genotypeDataset": 1,
+        "phenotypeDataset": 1,
+        "phenotype": "rand_qt",
+        "samples": "ALL",
+        "genomeBuild": "GRCh37",
+        "masks": ["AF < 0.01"]
+    })
+
+    assert resp.status_code == 200
+    assert resp.is_json
+
+    for group in resp.json["data"]["groups"]:
+        n_variants = len(group["variants"])
+        n_covar = len(group["covariance"])
+        assert n_covar == (n_variants * (n_variants + 1) / 2)
+
+    for variant in resp.json["data"]["variants"]:
+        assert variant["altFreq"] > 0
+        assert variant["pvalue"] > 0
+        assert variant["pvalue"] <= 1
