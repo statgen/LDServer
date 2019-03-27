c0a994d655d95e2951118ae9fd08619d20ece97e
Ryan Welch <welchr@umich.edu>
2019-03-27 09:46:00 -0400

    Sandbox update for debugging METSIM

--- core/src/Sandbox.cpp
+++ core/src/Sandbox.cpp
@@ -84,7 +84,82 @@ void test2() {
   auto samples2 = extract_samples("../../../data/chr21.test.sav");
 }
 
+void test3() {
+  // Genotype spec
+  uint32_t genotype_dataset_id = 2;
+  string genotype_file = "../../../data/METSIM/data/genotypes_chr22.vcf.gz";
+
+  // Phenotype spec
+  ColumnTypeMap ctmap;
+  ctmap.add("studyid", ColumnType::TEXT);
+  ctmap.add("hba1c", ColumnType::FLOAT);
+  ctmap.add("ldl", ColumnType::FLOAT);
+  ctmap.add("HOMA", ColumnType::FLOAT);
+  ctmap.add("t2d", ColumnType::CATEGORICAL);
+  ctmap.add("hdl", ColumnType::FLOAT);
+  ctmap.add("sbp", ColumnType::FLOAT);
+  ctmap.add("ins_30m", ColumnType::FLOAT);
+  ctmap.add("ins_fast", ColumnType::FLOAT);
+  ctmap.add("HOMA_B", ColumnType::FLOAT);
+  ctmap.add("coffee", ColumnType::FLOAT);
+  ctmap.add("glu_2h", ColumnType::FLOAT);
+  ctmap.add("chol", ColumnType::FLOAT);
+  ctmap.add("matsuda", ColumnType::FLOAT);
+  ctmap.add("glu_fast", ColumnType::FLOAT);
+  ctmap.add("ins_2h", ColumnType::FLOAT);
+  ctmap.add("tg", ColumnType::FLOAT);
+  ctmap.add("glu_30m", ColumnType::FLOAT);
+  ctmap.add("dbp", ColumnType::FLOAT);
+
+  string phenotype = "ldl";
+  uint64_t phenotype_nrows = 9376;
+  string phenotype_sample_col = "studyid";
+  uint32_t phenotype_dataset_id = 3;
+  string phenotype_file = "../../../data/METSIM/data/metsim.for_raremetal.pheno.tab";
+
+  // Region
+  string chrom = "22";
+  auto start = 50276998ul;
+  auto stop = 50357719ul;
+
+  // Masks for runner
+  uint64_t mask_id = 3;
+  Mask mask("../../../data/METSIM/data/mask.epacts.allchroms.gencode-exons-AF01.tab.gz", mask_id, VariantGroupType::GENE, GroupIdentifierType::ENSEMBL, chrom, start, stop);
+  vector<Mask> masks;
+  masks.emplace_back(mask);
+
+  // Config for runner
+  auto config = make_score_covariance_config();
+  config->chrom = chrom;
+  config->start = start;
+  config->stop = stop;
+  config->segment_size = 1000;
+  config->masks = masks;
+  config->sample_subset = "ALL";
+  config->genotype_files = {genotype_file};
+  config->genotype_dataset_id = genotype_dataset_id;
+  config->phenotype_file = phenotype_file;
+  config->phenotype_column_types = ctmap;
+  config->phenotype_dataset_id = phenotype_dataset_id;
+  config->phenotype = phenotype;
+  config->phenotype_nrows = phenotype_nrows;
+  config->phenotype_sample_column = phenotype_sample_col;
+  config->phenotype_delim = "\t";
+
+  config->pprint();
+
+  ScoreCovarianceRunner runner(config);
+  runner.run();
+
+  string json = runner.getPrettyJSON();
+  //cout << json << endl;
+
+  ofstream out("json.txt");
+  out << json;
+  out.close();
+}
+
 int main() {
-  test2();
+  test3();
   return 0;
 }
