c9d52d9f4390aa741f901b4c3338068fc9c05e74
Ryan Welch <welchr@umich.edu>
2019-03-29 14:45:21 -0400

    New test case for missing values

--- core/tests/LDServerTest.cpp
+++ core/tests/LDServerTest.cpp
@@ -361,6 +361,50 @@ TEST_F(LDServerTest, score_server) {
     }
 }
 
+TEST_F(LDServerTest, score_server_missing_values) {
+    // Load score statistics
+    auto goldstd = load_raremetal_scores("chr21.test.missing_values.RAND_QT.singlevar.score.txt");
+
+    // Load allele frequencies
+    auto goldfrq = load_variant_freqs("chr21.test.frq");
+
+    // LD server
+    LDServer ld_server(100);
+    LDQueryResult ld_result(INITIAL_RESULT_SIZE);
+    ld_result.limit = MAX_UINT32;
+    ld_server.set_file("chr21.test.vcf.gz");
+
+    // Score server
+    ScoreServer score_server(100);
+    ScoreStatQueryResult score_result(INITIAL_RESULT_SIZE);
+    score_result.limit = MAX_UINT32;
+    score_server.set_genotypes_file("chr21.test.vcf.gz", 1);
+
+    ColumnTypeMap ctmap;
+    ctmap.add("iid", ColumnType::TEXT);
+    ctmap.add("sex", ColumnType::CATEGORICAL);
+    ctmap.add("rand_binary", ColumnType::CATEGORICAL);
+    ctmap.add("rand_qt", ColumnType::FLOAT);
+
+    string phenotype_file = "chr21.test.missing_values.tab";
+    score_server.load_phenotypes_file(phenotype_file, ctmap, 2504, "\t", "iid", 1);
+    score_server.set_phenotype("rand_qt");
+
+    auto segments = make_shared_segment_vector();
+
+    ASSERT_TRUE(ld_server.compute_region_ld("21", 9411239, 9411793, correlation::COV, ld_result, "ALL", true, segments));
+    ASSERT_TRUE(score_server.compute_scores("21", 9411239, 9411793, score_result, "ALL", segments));
+    ASSERT_NEAR(goldstd->get_sigma2(), score_result.sigma2, 0.001);
+    for (auto&& score_res : score_result.data) {
+        auto gold = goldstd->get_record(score_res.variant);
+        ASSERT_NEAR(gold->u_stat, score_res.score_stat, 0.001);
+        ASSERT_NEAR(gold->pvalue, score_res.pvalue, 0.001);
+
+        auto gf = goldfrq->at(score_res.variant);
+        ASSERT_NEAR(gf, score_res.alt_freq, 0.001);
+    }
+}
+
 TEST_F(LDServerTest, score_covariance_runner) {
     string genotype_file = "chr22.test.vcf.gz";
     string phenotype_file = "chr22.test.tab";
--- data/make_rmw.sh
+++ data/make_rmw.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 raremetalworker --ped chr21.test.ped --dat chr21.test.dat --vcf chr21.test.vcf.gz --traitName RAND_QT --prefix chr21.test
+raremetalworker --ped chr21.test.missing_values.ped --dat chr21.test.dat --vcf chr21.test.vcf.gz --traitName RAND_QT --prefix chr21.test.missing_values
