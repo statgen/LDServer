9c21d56501cb83f4c37ed170179b665ddcde014c
Ryan Welch <welchr@umich.edu>
2019-04-03 12:20:44 -0400

    Update docker build script

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LDSERVER_VERSION=`git describe --tags --abbrev=11`
+LDSERVER_VERSION=`git describe --tags --abbrev=11 | sed 's/^v//' | sed 's/-g/-/'`
 GIT_SHA=`git rev-parse HEAD`
 BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%SZ'`
 
@@ -9,4 +9,6 @@ if [ `git rev-parse --abbrev-ref HEAD` != 'master' ]; then
 fi
 
 docker build -t ldserver:${LDSERVER_VERSION} --build-arg BUILD_DATE=${BUILD_DATE} --build-arg GIT_SHA=${GIT_SHA} --build-arg LDSERVER_VERSION=${LDSERVER_VERSION} . && \
-  docker tag ldserver:${LDSERVER_VERSION} ldserver:latest
+  docker tag ldserver:${LDSERVER_VERSION} ldserver:latest && \
+  docker tag ldserver:${LDSERVER_VERSION} statgen/ldserver:latest && \
+  docker tag ldserver:${LDSERVER_VERSION} statgen/ldserver:${LDSERVER_VERSION}
