d99ee62887ca38dd4c2759e8575383f49b3826fc
Ryan Welch <welchr@umich.edu>
2020-02-14 16:18:17 -0500

    Don't need to be on master to build image

--- bin/docker_build.sh
+++ bin/docker_build.sh
@@ -3,11 +3,6 @@ LDSERVER_VERSION=`git describe --tags --abbrev=11 | sed 's/^v//' | sed 's/-g/-/'
 GIT_SHA=`git rev-parse HEAD`
 BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%SZ'`
 
-if [ `git rev-parse --abbrev-ref HEAD` != 'master' ]; then
-  echo "Must be on master branch to build docker image"
-  exit 1
-fi
-
 docker build -t ldserver:${LDSERVER_VERSION} --build-arg BUILD_DATE=${BUILD_DATE} --build-arg GIT_SHA=${GIT_SHA} --build-arg LDSERVER_VERSION=${LDSERVER_VERSION} . && \
   docker tag ldserver:${LDSERVER_VERSION} ldserver:latest && \
   docker tag ldserver:${LDSERVER_VERSION} statgen/ldserver:latest && \
