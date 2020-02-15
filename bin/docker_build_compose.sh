e7279750bcbc08240b727170b65a35e59a9388e6
Ryan Welch <welchr@umich.edu>
2020-02-14 19:02:34 -0500

    Build local images when using compose

---
+++ bin/docker_build_compose.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export LDSERVER_VERSION=`git describe --tags --abbrev=11 | sed 's/^v//' | sed 's/-g/-/'`
+export GIT_SHA=`git rev-parse HEAD`
+export BUILD_DATE=`date -u +'%Y-%m-%dT%H:%M:%SZ'`
+
+docker-compose build "$@"
