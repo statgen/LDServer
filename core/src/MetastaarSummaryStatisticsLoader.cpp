0722d900ab97e654f6d999e292a71775d6f56e1f
Ryan Welch <welchr@umich.edu>
2021-07-27 19:24:17 -0400

    Parquet metadata validation

--- core/src/MetastaarSummaryStatisticsLoader.cpp
+++ core/src/MetastaarSummaryStatisticsLoader.cpp
@@ -60,8 +60,15 @@ MetastaarParquetMetadata read_parquet_metadata(const string& s) {
   extract_parquet_value(s, meta, "region_start", spstoull_uint64, pq_meta.region_start);
   extract_parquet_value(s, meta, "region_mid", spstoull_uint64, pq_meta.region_mid);
   extract_parquet_value(s, meta, "region_end", spstoull_uint64, pq_meta.region_end);
-
   extract_parquet_value(s, meta, "chrom", stos, pq_meta.chrom);
+
+  if (pq_meta.nrows < 0 || pq_meta.ncols < 0) throw_ldserver_exception("nrows or ncols in parquet metadata were invalid", {}, "parquet file was: %s", {s});
+  if (pq_meta.cov_maf_cutoff < 0 || pq_meta.cov_maf_cutoff > 1) throw_ldserver_exception("cov_maf_cutoff should be >= 0 and <= 1", {}, "parquet file was: %s", {s});
+  if (pq_meta.pos_start <= 0) throw_ldserver_exception("pos_start should be > 0", {}, "parquet file was: %s", {s});
+  if (pq_meta.pos_end <= 0) throw_ldserver_exception("pos_end should be > 0", {}, "parquet file was: %s", {s});
+  if (pq_meta.region_start <= 0) throw_ldserver_exception("region_start should be > 0", {}, "parquet file was: %s", {s});
+  if (pq_meta.region_mid <= 0) throw_ldserver_exception("region_mid should be > 0", {}, "parquet file was: %s", {s});
+  if (pq_meta.region_end <= 0) throw_ldserver_exception("region_end should be > 0", {}, "parquet file was: %s", {s});
 
   return pq_meta;
 }
